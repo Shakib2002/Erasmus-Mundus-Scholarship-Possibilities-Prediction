ad7f30c5411e092e27dd789175f5525f4570229d
Shakib2002 <hello.sanzox@gmail.com>
2026-07-19 18:27:46 +0600

    Auto-commit: 2026-07-19 06:27:45 PM +06:00

--- demo.py
+++ demo.py
@@ -1,3 +1,10 @@
+import sys
 from scholarship_prediction.logger import logging
+from scholarship_prediction.exception import ScholarshipException
 
-logging.info("This is a test log")
+
+try:
+    raise Exception("This is a test exception")
+except Exception as e:
+    logging.info("This is a test log")
+    raise ScholarshipException(e, sys)
